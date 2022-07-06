d7b8e15f0ade2769248361765259e50ba71e556d
eddyralston <eddyralston@outlook.com>
2022-07-06 00:58:31 -0600

    Add files via upload

--- Component.js
+++ Component.js
@@ -1,44 +1,48 @@
-const objectMap = function (obj, callback) {
+const objectMapValues = function (obj) {
     var result = {}
-    for (var prop in obj) result[prop] = callback(obj[prop])
+    for (var prop in obj) result[prop] = obj[prop].value
     return result
 }
+
+const query = function (parent, attr, callback) {
+    var query = parent.querySelectorAll('[' + attr + ']');
+    var len = query.length;
+    for (var i = 0; i < len; i++) callback(query[i], query[i].getAttribute(attr));
+}
+
+const queryToObject = (element,attr) => {
+    var obj = {}
+    query(element, attr, (child, attr) => obj[attr] = child)
+    return obj
+}
+
+const constructMethods = ({data,child,methods}) => ({
+    get data() {return objectMapValues(data)},
+    get child() {return child},
+    function: (name, method) => methods[name] = method
+});
+
+const queryMethods = (element,methods) => query(element, 'on', (child, attr) => {
+    var [event, name] = attr.split(':')
+    child.addEventListener(event, () => methods[name]())
+})
+
 const html = function (str) {
     var el = document.createElement('div')
     el.innerHTML = str
     return el.firstElementChild
 }
-const queryChildren = function (parent, attr, callback) {
-    var query = parent.querySelectorAll('[' + attr + ']');
-    var len = query.length
-    for (var i = 0; i < len; i++) callback(query[i], query[i].getAttribute(attr))
-}
 
-function Component(obj) {
-    var self = {
-        element: html(obj.html),
-        data: {},
-        methods: {},
-        child: {}
+const Component = (obj) => {
+    var element = html(obj.html)
+    var props = {
+        data : queryToObject(element,'data'),
+        child : queryToObject(element,'child'),
+        methods : {}
     }
-
-    queryChildren(self.element, 'data', (child, attr) => self.data[attr] = child)
-
-    queryChildren(self.element, 'child', (child, attr) => self.child[attr] = child)
-    
-    queryChildren(self.element, 'on', (child, attr) => {
-        var [event, name] = attr.split(':')
-        child.addEventListener(event, () => self.methods[name]())
-    })
-
-    obj.construct({
-        get data() {
-            return objectMap(self.data, (child) => child.value)
-        },
-        function: (name, method) => self.methods[name] = method,
-        child:self.child
-    })
-    return self.element
+    queryMethods(element,props.methods)
+    obj.construct(constructMethods(props))
+    return element
 }
 
 export {Component,html}
